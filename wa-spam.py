e7717781d88b0106d3d065e5eb9c49e2c09d15da
Subash Mylraj <subashmylraj@gmail.com>
2020-10-19 22:29:07 +0300

    Added wa-spam.py

---
+++ wa-spam.py
@@ -0,0 +1,56 @@
+from splinter import Browser
+from time import sleep
+import argparse
+
+parser = argparse.ArgumentParser(description="Spam your friends or enemies on whatsapp!")
+parser.add_argument('chat_name', type=str, help="Name of the chat you want to spam")
+parser.add_argument('msg', type=str, help="Message to be sent")
+parser.add_argument('count', type=int, help="Number of messages to be sent")
+parser.add_argument('-n', action='store_true', help="Explicitly set the network proxy type to: no proxy")
+parser.add_argument('--waittime', type=int, default=10, help="Change the time that the application waits for you to login. Default: 10 seconds")
+
+args = parser.parse_args()
+
+input("Press enter when you are ready with the whatsapp scanner on your phone!\nYou will have 10 secs to scan the QR code.\n")
+
+browser = Browser()
+
+if(args.n):
+
+	browser.visit("about:config")
+	script = """
+	var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+	.getService(Components.interfaces.nsIPrefBranch);
+
+	prefs.setIntPref("network.proxy.type", 0);
+	"""
+	browser.execute_script(script.format("ProxyIP", "PORT"))
+
+
+url = "https://web.whatsapp.com/"
+browser.visit(url)
+
+# time to login to whatsapp using QR code
+sleep(args.waittime)
+
+# find the chat
+chat_search = browser.find_by_xpath("/html/body/div[1]/div/div/div[3]/div/div[1]/div/label/div/div[2]").first
+chat_search.type(args.chat_name)
+
+# press the chat
+browser.find_by_xpath('//*[@title="'+args.chat_name+'"]').click()
+
+# send message
+for i in range(args.count):
+	chatbox = browser.find_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]").first
+	chatbox.type(args.msg)
+
+	send_button = browser.find_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button").first.click()
+
+
+#logout from whatsapp
+browser.find_by_xpath("/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/div").first.click()
+browser.find_by_xpath("/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/span/div/ul/li[7]/div").first.click()
+
+#close window
+browser.quit()
