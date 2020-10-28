a70263d996941d7ab0e5fbe7d71a0b9766e60426
Subash Mylraj <subashmylraj@gmail.com>
2020-10-28 19:34:33 +0300

    Testing eol bug fix

--- wa-spam.py
+++ wa-spam.py
@@ -19,34 +19,34 @@ if(args.file == None and args.msg == None):
 print("You will have 10 secs to scan the QR code.")
 input("Press enter when you are ready with the whatsapp scanner on your phone!")
 
-browser = Browser()
+# browser = Browser()
 
-if(args.n):
+# if(args.n):
 
-	browser.visit("about:config")
-	script = """
-	var prefs = Components.classes["@mozilla.org/preferences-service;1"]
-	.getService(Components.interfaces.nsIPrefBranch);
+# 	browser.visit("about:config")
+# 	script = """
+# 	var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+# 	.getService(Components.interfaces.nsIPrefBranch);
 
-	prefs.setIntPref("network.proxy.type", 0);
-	"""
-	browser.execute_script(script.format("ProxyIP", "PORT"))
+# 	prefs.setIntPref("network.proxy.type", 0);
+# 	"""
+# 	browser.execute_script(script.format("ProxyIP", "PORT"))
 
 
-url = "https://web.whatsapp.com/"
-browser.visit(url)
+# url = "https://web.whatsapp.com/"
+# browser.visit(url)
 
-# time to login to whatsapp using QR code
-sleep(args.waittime)
+# # time to login to whatsapp using QR code
+# sleep(args.waittime)
 
-# find the chat
-chat_search = browser.find_by_xpath("/html/body/div[1]/div/div/div[3]/div/div[1]/div/label/div/div[2]").first
-chat_search.type(args.chat_name)
+# # find the chat
+# chat_search = browser.find_by_xpath("/html/body/div[1]/div/div/div[3]/div/div[1]/div/label/div/div[2]").first
+# chat_search.type(args.chat_name)
 
-# press the chat
-browser.find_by_xpath('//*[contains(@title, "'+args.chat_name+'")]').click()
+# # press the chat
+# browser.find_by_xpath('//*[contains(@title, "'+args.chat_name+'")]').click()
 
-confirmation = input("is this the chat you want to send the messages to?\nEnter yes or no:")
+# confirmation = input("is this the chat you want to send the messages to?\nEnter yes or no:")
 if(confirmation == 'yes'):
 
 	# send message
@@ -59,15 +59,15 @@ if(confirmation == 'yes'):
 	else:
 		msg = args.msg
 
-	for i in range(args.count):
-		chatbox = browser.find_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]").first
-		chatbox.type(msg)
+	# for i in range(args.count):
+	# 	chatbox = browser.find_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]").first
+	# 	chatbox.type(msg)
 
-		send_button = browser.find_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button").first.click()
+	# 	send_button = browser.find_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button").first.click()
 
 #logout from whatsapp
-browser.find_by_xpath("/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/div").first.click()
-browser.find_by_xpath("/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/span/div/ul/li[7]/div").first.click()
+# browser.find_by_xpath("/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/div").first.click()
+# browser.find_by_xpath("/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/span/div/ul/li[7]/div").first.click()
 
-#close window
-browser.quit()
+# #close window
+# browser.quit()
